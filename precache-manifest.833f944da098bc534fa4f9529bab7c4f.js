fcd2bf7fa93cc84fbb19b7846a7e800ae3e89f3d
ilya-velychko <ilya.velychko@gotoinc.co>
2020-05-30 20:10:52 +0300

    Updates

---
+++ precache-manifest.833f944da098bc534fa4f9529bab7c4f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77247852a21ef139bb68847b588b4201",
+    "url": "/emarketpolo/index.html"
+  },
+  {
+    "revision": "06924054ed0f15558d4b",
+    "url": "/emarketpolo/static/css/main.0ff3de09.chunk.css"
+  },
+  {
+    "revision": "d489cd435ced59c74840",
+    "url": "/emarketpolo/static/js/2.eb822f77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emarketpolo/static/js/2.eb822f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06924054ed0f15558d4b",
+    "url": "/emarketpolo/static/js/main.97b31903.chunk.js"
+  },
+  {
+    "revision": "5b15ac1e6ed105dcc511",
+    "url": "/emarketpolo/static/js/runtime-main.a0c2bb18.js"
+  },
+  {
+    "revision": "062d89c3be20518d9240aab0a3257551",
+    "url": "/emarketpolo/static/media/help.062d89c3.svg"
+  },
+  {
+    "revision": "8fee74e213a538dbe5c614d5632d5914",
+    "url": "/emarketpolo/static/media/package.8fee74e2.svg"
+  },
+  {
+    "revision": "00e1a3d11c60803df5bd81d5f25e2e3f",
+    "url": "/emarketpolo/static/media/polo-logo.00e1a3d1.svg"
+  },
+  {
+    "revision": "eda0d27e17ff5850570afeb2b2b2be83",
+    "url": "/emarketpolo/static/media/search.eda0d27e.svg"
+  }
+]);
